1f5bf95f741a7eb7a980ca977851b4ddebd4f639
Chris Hillery <ceej@couchbase.com>
2014-07-24 13:46:08 -0700

    Add universe repo; fix (and perform) apt-get update

--- ubuntu/missing-dir.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Would really like to call this before the docker image is created,
-# but mkimage.sh doesn't support that for debootstrap. I leave this
-# file here as a reminder.
-prefix=$1
-mkdir $prefix/var/lib/apt/lists/partial
